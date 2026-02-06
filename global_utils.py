7e46ed79e03acb67be1eb861a0606b1bd855a6b6
LucaPotenza <potenza171@gmail.com>
2026-02-06 21:14:55 +0100

    Update global_utils.py

--- global_utils.py
+++ global_utils.py
@@ -8,7 +8,7 @@ import random
 def set_seed(seed: int = 0):
     """Make runs reproducible (as much as possible)."""
     random.seed(seed)
-    np.random.seed(seed)
+    #np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
